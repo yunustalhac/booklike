bc994bb3c0d04e137f3ca67d0e19d182e8f5338b
yunustalhac <yunustalhacoban@gmail.com>
2023-08-29 21:31:40 +0300

    bismillah

--- booklike/src/main.js
+++ booklike/src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { appAxios } from "@/utils/appAxios";
+import "@/assets/style.css";
 
-createApp(App).mount('#app')
+import appHeader from "@/components/Shared/appHeader";
+import appBookmarkList from "@/components/Shared/appBookmarkList/indexPage.vue";
+
+import io from "socket.io-client";
+const socket = io("http://localhost:2018");
+
+const app = createApp(App);
+app.component("AppHeader", appHeader);
+app.component("appBookmarkList", appBookmarkList);
+app.use(router);
+app.use(store);
+app.config.globalProperties.$appAxios = appAxios;
+app.config.globalProperties.$log = console.log;
+app.config.globalProperties.$socket = socket;
+app.mount("#app");
