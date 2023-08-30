47e57ffac55f0e91f12c899c66996853c599209e
yunustalhac <yunustalhacoban@gmail.com>
2023-08-30 12:53:59 +0300

    bismillah

--- booklike/src/main.js
+++ booklike/src/main.js
@@ -1,14 +1,16 @@
-import { createApp } from "vue";
+import {createApp} from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { appAxios } from "@/utils/appAxios";
+import {appAxios} from "@/utils/appAxios";
 import "@/assets/style.css";
 
 import appHeader from "@/components/Shared/appHeader";
 import appBookmarkList from "@/components/Shared/appBookmarkList/indexPage.vue";
 
 import io from "socket.io-client";
+
+
 const socket = io("http://localhost:2018");
 
 const app = createApp(App);
@@ -19,4 +21,6 @@ app.use(store);
 app.config.globalProperties.$appAxios = appAxios;
 app.config.globalProperties.$log = console.log;
 app.config.globalProperties.$socket = socket;
+app.provide("appAxios", appAxios);
+app.provide("socket", socket);
 app.mount("#app");
--- booklike/src/views/loginPage.vue
+++ booklike/src/views/loginPage.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="login_register_container">
     <h3 class="text-2xl text-center mb-3">Giriş Yap</h3>
-    <input v-model="userData.username" type="text" placeholder="Kullanıcı Adı" class="input mb-3" />
-    <input v-model="userData.password" type="password" placeholder="Şifre" class="input mb-3" />
+    <input v-model="userData.username" type="text" placeholder="Kullanıcı Adı" class="input mb-3"/>
+    <input v-model="userData.password" type="password" placeholder="Şifre" class="input mb-3"/>
     <button @click="onSubmit" class="default-button">Giriş yap</button>
     <span class="text-center mt-3 text-sm">
       Üye değilim,
@@ -12,33 +12,35 @@
     </span>
   </div>
 </template>
-<script>
+
+
+<script setup>
+import {inject, ref} from "vue";
+import {useStore} from "vuex";
+import {useRouter} from "vue-router"
 import CryptoJS from "crypto-js";
-export default {
-  data() {
-    return {
-      userData: {
-        username: null,
-        password: null
-      }
-    };
-  },
-  methods: {
-    onSubmit() {
-      const password = CryptoJS.HmacSHA1(this.userData.password, this.$store.getters._saltKey).toString();
-      this.$appAxios
-        .get(`/users?username=${this.userData.username}&password=${password}`)
-        .then(login_response => {
-          if (login_response?.data?.length > 0) {
-            this.$store.commit("setUser", login_response?.data[0]);
-            this.$router.push({ name: "HomePage" });
-          } else {
-            alert("Böyle bir kullanıcı bulunamadı...");
-          }
-        })
-        .catch(e => console.log(e));
-      // .finally(() => this.loader = false)
-    }
-  }
-};
+
+const store = useStore()
+const router = useRouter()
+const appAxios = inject("appAxios")
+const userData = ref({
+  username: null,
+  password: null,
+})
+const onSubmit = () => {
+  const password = CryptoJS.HmacSHA1(userData.value.password, store.getters._saltKey).toString();
+  appAxios
+      .get(`/users?username=${userData.value.username}&password=${password}`)
+      .then(login_response => {
+        if (login_response?.data?.length > 0) {
+          store.commit("setUser", login_response?.data[0]);
+          router.push({name: "HomePage"});
+        } else {
+          alert("Böyle bir kullanıcı bulunamadı...");
+        }
+      })
+      .catch(e => console.log(e));
+  // .finally(() => this.loader = false)
+}
+
 </script>
